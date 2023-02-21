2e33158babe2655dc20a26b90f2969084fd391c1
Lucas Voigt <lucas.voigt@web.de>
2023-02-21 22:09:28 +0000

    Item Model created, database deleted to accommodate newly added columns.

--- budget/balance/models.py
+++ budget/balance/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+types = (
+    ("Income", "Income"),
+    ("Expense", "Expense"),
+    ("Loan", "Loan")
+)
+
+repeat_patterns = (
+    ("monthly", "monthly"),
+    ("weekly", "weekly"),
+    ("every two weeks", "every two weeks"),
+    ("every three weeks", "every three weeks"),
+    ("every four weeks", "every four weeks"),
+)
+
+today = datetime.today()
+
+
+class Item(models.Model):
+    type = models.CharField(max_length=10,
+                            choices=types,
+                            default="Expense"
+                            )
+
+    purpose = models.CharField(max_length=200)
+    amount = models.FloatField(max_length=6)
+    due_date = models.DateField()
+    repeat_pattern = models.CharField(max_length=30,
+                                      choices=repeat_patterns,
+                                      default="monthly")
+    website = models.URLField()
+    email = models.EmailField()
+    telefone = models.PositiveBigIntegerField()
+    end_date = models.DateField()
+    date_added = models.DateField(default=today.strftime(
+        "%d/%m/%Y"), editable=False, auto_created=True)
+
+    def __str__(self):
+        return f"{self.type}"
